a584c58a1c4752244b267f374b11e07d07ec2244
Tarang Patel <tarangp@me.com>
2014-09-20 22:12:36 +0300

    Bring up to date

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
-        summary: "GeoIP-lite Npm wrapper"
+    summary: "GeoIP-lite Npm wrapper",
+    name: "servicelocale:geoip",
+	version: "0.1.2"
 });
 
 Npm.depends({
-        'geoip-lite': "1.1.3"
+	'geoip-lite': "1.1.3"
 });
 
 Package.on_use(function(api) {
+    api.versionsFrom("METEOR@0.9.2");
     api.add_files('lib.js', 'server');
     api.export("GeoIP");
 });
